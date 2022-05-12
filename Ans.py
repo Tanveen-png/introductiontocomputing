87825acf668d70597ff079ec07d434a7595e48b0
Tanveen-png <105067563+Tanveen-png@users.noreply.github.com>
2022-05-12 23:47:18 +0530

    Add files via upload

---
+++ Ans.py
@@ -0,0 +1,61 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/tanveen kaur/Assigment.py =================
+enter the number: 12
+001100
+select operation
+1.Add
+2.subtract
+3.Multiply
+4.Divide
+Enter the choice(1/2/3/4)1
+Enter first number:1
+Enter the second number:2
+1.0 + 2.0 = 3.0
+Next Calculation (Y/N):N
+
+a 35
+Enter the number:12
+b 66.0
+Enter the number:12
+c 1964.16
+enter angle12
+enter angle13
+d 3.265510390436714
+enter number12
+enter number13
+enter number14
+enter number15
+e 1.0
+
+a)
+0
+1
+2
+3
+4
+b)
+3
+4
+5
+6
+7
+8
+9
+c)
+4
+7
+10
+d)
+15
+13
+11
+9
+7
+e)
+
+Enter the no of hydrogen atom: 2
+Enter the no of carbohydrate atom: 1
+Enter the no of oxygen atom: 1
+Molecular Weight:  30.025979999999997
