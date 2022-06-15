c07866c65b3b3a6bca5330274558ca06d6bc2b6b
Tanveen-png <105067563+Tanveen-png@users.noreply.github.com>
2022-06-15 22:50:13 +0530

    Add files via upload

---
+++ assignment 5 output.py
@@ -0,0 +1,127 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/tanveen kaur/Desktop/assignment 5 input.py =========
+Ans 1)
+Enter string: tanveem
+The original string  is :  tanveem
+The reversed string using a while loop is :  meevnat
+
+Ans 2)
+Enter the number: 4
+Enter the range: 20
+The numbers divisible are: 
+0
+4
+8
+12
+16
+
+Ans3
+Enter first side: 2
+Enter second side: 3
+Enter third side: 4
+The area of the triangle is  2.9047375096555625
+
+Ans4
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+
+Ans6
+Please, Enter the Lowest Range Value: 20
+Please, Enter the Upper Range Value: 30
+The Prime Numbers in the range are: 
+23
+29
+
+Ans7
+77
+154
+231
+308
+385
+462
+
+Ans8
+Enter value: 30
+Enter value: 12
+Enter value: 12
+Enter value: -2
+Enter value: 0
+Enter value: 22
+Enter value: 23
+Enter value: 34
+Enter value: 54
+Enter value: 43
+positive numbers
+30
+12
+12
+22
+23
+34
+54
+43
+negative numbers
+-2
+odd numbers
+23
+43
+even numbers
+30
+12
+12
+-2
+0
+22
+34
+54
+Number of times each number occurs in the List
+Count of 30 = 1
+Count of 12 = 2
+Count of 12 = 2
+Count of -2 = 1
+Count of 0 = 1
+Count of 22 = 1
+Count of 23 = 1
+Count of 34 = 1
+Count of 54 = 1
+Count of 43 = 1
+
+Ans9
+
+length of list: 10
+Enter value: 4
+Enter value: 6
+Enter value: 5
+Enter value: 5
+Enter value: 4
+Enter value: 4
+Enter value: 4
+Enter value: 3
+Enter value: 5
+Enter value: 6
+Count of 4 = 4
+Count of 6 = 2
+Count of 5 = 3
+Count of 5 = 3
+Count of 4 = 4
+Count of 4 = 4
+Count of 4 = 4
+Count of 3 = 1
+Count of 5 = 3
+Count of 6 = 2
