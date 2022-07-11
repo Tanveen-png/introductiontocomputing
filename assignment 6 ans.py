f85848d386ae81c66a8786f05d8e97e68851b10b
Tanveen-png <105067563+Tanveen-png@users.noreply.github.com>
2022-07-11 11:53:26 +0530

    Add files via upload

---
+++ assignment 6 ans.py
@@ -0,0 +1,34 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/tanveen kaur/OneDrive/Documents/assignment 6.py ======
+enter a number to check whether its perfect: 6
+divisors of  6  are  [1, 2, 3, 6]
+6  is a perfect number.
+enter a word, phrase or sentence: tree
+eert
+entered string is not a palindrome
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+Enter a sentence: how are you?
+entered sentence is not a palgram
+enter a hyphen separated sentence: The quick brown fox jumps over the lazy dog
+The quick brown fox jumps over the lazy dog
+student name:  Tanveen kaur
+student branch:  mechanical
+student id:  21107062
+True
+False
+True
+False
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+0 -1 1
+2 -3 1
+Enter the sequence of parantheses : hiutr
+hiutr - is unbalanced
