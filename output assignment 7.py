2d1255f7da4638672347fa57974851599ef10215
Tanveen-png <105067563+Tanveen-png@users.noreply.github.com>
2022-07-17 23:45:40 +0530

    Add files via upload

---
+++ output assignment 7.py
@@ -0,0 +1,10 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: C:/Users/tanveen kaur/q.py =====================
+[0, 1, 3, 23, 51]
+[2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]
+Element 5 occurs 3 times
+[4, 5, 2, 6, 3, 7]
+[2, 3, 4, 5, 6, 7]
+[2, 3, 4, 5, 6, 7]
